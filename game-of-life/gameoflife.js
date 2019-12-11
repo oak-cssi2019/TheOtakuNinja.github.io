7d09268b2b10174dc095bc2e69bfa2e561047453
Jacob <JacobS@CSedge.org>
2019-12-10 18:29:37 -0800

    done

--- game-of-life/gameoflife.js
+++ game-of-life/gameoflife.js
@@ -1,137 +1,230 @@
-console.log("YARE YARE DAZE");
 class GameOfLife {
-  constructor(){
+  constructor() {
   }
 
   next(shape) {
+		let neighbours = {};
+
+    for(let i = 0; i < shape.length; i++) {
+      let cell = shape[i];
+      let key;
+
+      key = 'c'+(cell[0]-1)+','+(cell[1]-1);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0]-1, cell[1]-1]};
+			}
+
+			key = 'c'+(cell[0])+','+(cell[1]-1);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0], cell[1]-1]};
+			}
+
+			key = 'c'+(cell[0]+1)+','+(cell[1]-1);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0]+1, cell[1]-1]};
+			}
+
+			key = 'c'+(cell[0]-1)+','+(cell[1]);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0]-1, cell[1]]};
+			}
+
+			key = 'c'+(cell[0]+1)+','+(cell[1]);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0]+1, cell[1]]};
+			}
+
+			key = 'c'+(cell[0]-1)+','+(cell[1]+1);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0]-1, cell[1]+1]};
+			}
+
+			key = 'c'+(cell[0])+','+(cell[1]+1);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0], cell[1]+1]};
+			}
+
+			key = 'c'+(cell[0]+1)+','+(cell[1]+1);
+			if (neighbours[key]) {
+				neighbours[key].n++;
+			} else {
+				neighbours[key] = {n: 1, cell: [cell[0]+1, cell[1]+1]};
+			}
+		}
+
+    for(let i = 0; i < shape.length; i++) {
+      let cell = shape[i];
+			let key = 'c' + cell[0] + ',' + cell[1];
+			if (neighbours[key]) {
+				neighbours[key].populated = true;
+			}
+		};
+
+		let nextShape = [];
+		for (let key in neighbours) {
+			if ((neighbours[key].n == 2 && neighbours[key].populated) || neighbours[key].n == 3) {
+				nextShape.push(neighbours[key].cell);
+			}
+		}
+
+		return nextShape;
   }
 }
 
 class Canvas {
-  constructor(container){
-    let canvasElement = document.createElement("canvas");
+  constructor(container) {
+    let canvasElement = document.createElement('canvas');
     this.obj = canvasElement;
     this.pixelWidth = 803;
-    canvasElement.width = this.pixelWidth;
     this.pixelHeight = 506;
+    canvasElement.width = this.pixelWidth;
     canvasElement.height = this.pixelHeight;
     container.appendChild(canvasElement);
-    this.ctx = this.obj.getContext("2d");
+    this.ctx = canvasElement.getContext('2d');
     this.setGridSize(11);
   }
 
-  draw(cells){
-    let ctx = this.ctx;
+  draw(cells) {
+    this.ctx.fillStyle = "#7e7e7e";
+    this.ctx.lineWidth = 1;
+    this.ctx.fillRect (0, 0, this.pixelWidth, this.pixelHeight);
+    this.ctx.strokeStyle = "#999";
 
-
-    ctx.fillStyle = "#7e7e7e";
-    ctx.lineWidth = 1;
-    ctx.fillRect(0,0, this.pixelWidth, this.pixelHeight);
-    ctx.strokeStyle = "#999";
-
-    for (let n = this.cellSize; n < this.pixelWidth; n += this.cellSize) {
-      ctx.beginPath();
-      ctx.moveTo(n + .5 , 0);
-      ctx.lineTo(n + .5 , this.pixelHeight);
-      ctx.stroke();
+    for(let n = this.cellSize; n < this.pixelWidth; n += this.cellSize) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(n + 0.5, 0);
+      this.ctx.lineTo(n + 0.5, this.pixelHeight);
+      this.ctx.stroke();
+    }
+    for(let n = this.cellSize; n < this.pixelHeight; n += this.cellSize) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, n + 0.5);
+      this.ctx.lineTo(this.pixelWidth, n + 0.5);
+      this.ctx.stroke();
     }
 
-    for (let n = this.cellSize; n < this.pixelHeight; n += this.cellSize) {
-      ctx.beginPath();
-      ctx.moveTo(0, n + .5);
-      ctx.lineTo(this.pixelWidth , n + 0.5);
-      ctx.stroke();
-    }
-
-
-  }
-
-  click(fn){
-
-  }
-
-  getDimension(){
-
-  }
-
-  getGridSize(){
-
-  }
-
-  setGridSize(gridSize){
-    this.cellSize = gridSize;
-    this.width = Math.floor(this.pixelWidth/this.cellSize);
-    this.height = Math.floor(this.pixelHeight/this.cellSize);
-    // this.cellSize = size;
-  }
-
-}
-
-class Shape {
-  constructor(Canvas){
-      this.canvas = canvas;
-      this.current = [];
-      this.collection = {};
-  }
-
-  get(){
-
-  }
-
-  set(shape){
-
-  }
-
-  copy(shape){
-
-  }
-
-  redraw(){
-
-  }
-
-  offset(dx, dy){
-
-  }
-
-  toggle(cell){
-
-  }
-}
-
-
-class Controls {
-  constructor(Canvas, Shape, GameOfLife){
-       this.canvas = Canvas;
-       this.shape = Shape;
-       this.gameoflife = GameOfLife;
-       this.started = false;
-       this.timer = null;
-       this.generation = 0;
-  }
-
-  init(shapes){
-    this.canvas.click((event) => {
+    this.ctx.fillStyle = "yellow";
+    this.ctx.lineWidth = 1;
+    cells.forEach((cell, i) => {
+      this.ctx.fillRect(
+        cell[0] * this.cellSize + 1,
+        cell[1] * this.cellSize + 1,
+        this.cellSize - 1,
+        this.cellSize - 1);
     });
   }
 
-  setGenerations(gen){
-
+  click(fn) {
+    this.obj.addEventListener('click', (evt) => {
+      let rect = canvas.obj.getBoundingClientRect();
+      let left = rect.left;
+      let top = rect.top;
+      let cellSize = canvas.cellSize;
+      let clickEvent = {};
+      clickEvent.cellX = Math.floor((evt.clientX - left) / cellSize);
+      clickEvent.cellY = Math.floor((evt.clientY - top) / cellSize);
+      fn(clickEvent);
+    });
   }
 
-  animate(){
-
+  getDimension() {
   }
 
-  next(){
+  getGridSize() {
+  }
 
+  setGridSize(size) {
+    this.cellSize = size;
   }
 }
 
-let canvasElement = document.getElementById("canvas-div");
+class Shape {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.current = [];
+    this.collection = {};
+  }
+
+  get() {
+    return this.current;
+  }
+
+  set(shape)  {
+    this.current = shape;
+  }
+
+  copy(shape) {
+  }
+
+  redraw() {
+    this.canvas.draw(this.current);
+  }
+
+  center() {
+  }
+
+  offset(dx, dy) {
+  }
+
+  toggle(cell) {
+    this.current.push(cell);
+    this.redraw();
+  }
+}
+
+class Controls {
+  constructor(canvas, shape, gameOfLife) {
+    this.canvas = canvas;
+    this.shape = shape;
+    this.gameOfLife = gameOfLife;
+    this.generation = 0;
+  }
+
+  init(shapes) {
+    this.canvas.click((evt) => {
+      this.shape.toggle([evt.cellX, evt.cellY]);
+    });
+
+
+		document.getElementById('next').addEventListener('click', () => {
+			this.next();
+		});
+  }
+
+  setGeneration(gen) {
+  }
+
+  animate() {
+  }
+
+  next() {
+		let shapeData = this.shape.get();
+		let newShapeData = this.gameOfLife.next(shapeData);
+		this.shape.set(newShapeData);
+		this.shape.redraw();
+  }
+}
+
+let canvasElement = document.getElementById('canvas-div');
 let canvas = new Canvas(canvasElement);
 let shape = new Shape(canvas);
-let gameoflife = new GameOfLife();
-let control = new Controls(canvas, shape, gameoflife);
+let gameOfLife = new GameOfLife();
+let controls = new Controls(canvas, shape, gameOfLife);
 
-canvas.draw();
+controls.init(shape.collection);
+//controls.shape.center();
+controls.shape.redraw();
